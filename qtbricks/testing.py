2565c23c39d4689d289306458b53219e21af693c
Till Biskup <till@till-biskup.de>
2024-01-10 20:04:03 +0100

    Add context managers and test case to test for Qt signals

--- qtbricks/testing.py
+++ qtbricks/testing.py
@@ -12,7 +12,9 @@ This module contains a growing series of helper classes and functions
 designed to make your test code simpler and easier to read.
 """
 
-from PySide6 import QtCore, QtTest
+import unittest
+
+from PySide6 import QtCore, QtTest, QtWidgets
 
 
 def qtest_enter_text(widget=None, text=""):
@@ -41,3 +43,166 @@ def qtest_enter_text(widget=None, text=""):
     widget.clear()
     QtTest.QTest.keyClicks(widget, text)
     QtTest.QTest.keyPress(widget, QtCore.Qt.Key.Key_Return)
+
+
+class SignalReceiver:
+    """
+    Context manager class detecting whether a QSignal has been emitted.
+
+    Adapted from https://stackoverflow.com/a/48128768
+    """
+
+    def __init__(self, test, signal, *args):
+        self.test = test
+        self.signal = signal
+        self.called = False
+        self.expected_args = args
+        self.actual_args = None
+
+    def slot(self, *args):
+        """
+        Slot connected to the signal to test for.
+
+        If the signal is emitted, the attribute :attr:`called` is set to True.
+
+        Parameters
+        ----------
+        args
+            Parameters passed by the emitted signal.
+
+        """
+        self.actual_args = args
+        self.called = True
+
+    def __enter__(self):
+        """Entrance of the context manager."""
+        self.signal.connect(self.slot)
+
+    def __exit__(self, exception, msg, traceback):
+        """
+        Exit of the context manager.
+
+        Parameters
+        ----------
+        exception
+            Exception that may have been raised.
+
+        msg : :class:`str`
+            Message to pass to the exception
+
+        traceback
+            Call trace leading to the exception
+
+        """
+        if exception:
+            raise exception(msg)
+        self.test.assertTrue(self.called, "Signal not called!")
+        if self.actual_args:
+            self.test.assertEqual(
+                self.expected_args,
+                self.actual_args,
+                f"""Signal arguments don't match!
+                actual:   {self.actual_args}
+                expected: {self.expected_args}""",
+            )
+
+
+class SignalNotReceiver:
+    """
+    Context manager class detecting whether a QSignal has not been emitted.
+
+    Adapted from https://stackoverflow.com/a/48128768
+    """
+
+    def __init__(self, test, signal):
+        self.test = test
+        self.signal = signal
+        self.called = False
+
+    def slot(self):
+        """
+        Slot connected to the signal to test for.
+
+        If the signal is emitted, the attribute :attr:`called` is set to True.
+        """
+        self.called = True
+
+    def __enter__(self):
+        """Entrance of the context manager."""
+        self.signal.connect(self.slot)
+
+    def __exit__(self, exception, msg, traceback):
+        """
+        Exit of the context manager.
+
+        Parameters
+        ----------
+        exception
+            Exception that may have been raised.
+
+        msg : :class:`str`
+            Message to pass to the exception
+
+        traceback
+            Call trace leading to the exception
+
+        """
+        if exception:
+            raise exception(msg)
+        self.test.assertFalse(self.called, "Signal called!")
+
+
+class TestCaseUsingQSignals(unittest.TestCase):
+    """
+    Test class for testing whether a QSignal has been emitted.
+
+    Adapted from https://stackoverflow.com/a/48128768
+    """
+
+    def setUp(self):
+        """Create the QApplication instance"""
+        _instance = QtWidgets.QApplication.instance()
+        if not _instance:
+            _instance = QtWidgets.QApplication([])
+        self.app = _instance
+
+    def tearDown(self):
+        """Delete the reference owned by self"""
+        del self.app
+
+    def assertSignalReceived(self, signal, args):  # noqa N802
+        """
+        Check whether signal has been received.
+
+        Parameters
+        ----------
+        signal
+            Signal to check for
+
+        args
+            Parameters passed by the emitted signal.
+
+        Returns
+        -------
+        answer : :class:`bool`
+            Result of the test
+
+        """
+        return SignalReceiver(self, signal, args)
+
+    def assertSignalNotReceived(self, signal):  # noqa N802
+        """
+        Check whether signal has not been received.
+
+        Parameters
+        ----------
+        signal
+            Signal to check for
+
+        Returns
+        -------
+        answer : :class:`bool`
+            Result of the test
+
+        """
+        return SignalNotReceiver(self, signal)
